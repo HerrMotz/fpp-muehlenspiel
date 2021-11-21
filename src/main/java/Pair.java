dda9add4e1cfbb86fc37bc49a0a2e3bf80b5338f
Daniel Motz <daniel.motz@mandant3.de>
2021-11-21 11:08:11 +0100

    add comparing methods for Pair

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Pair<L, R> {
     private L left;
     private R right;
@@ -23,5 +25,18 @@ public class Pair<L, R> {
         return "(" + left +
                 "," + right +
                 ')';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(getLeft(), pair.getLeft()) && Objects.equals(getRight(), pair.getRight());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLeft(), getRight());
     }
 }
